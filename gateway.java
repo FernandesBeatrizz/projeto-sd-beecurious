0b866fbfb4d54adfff44bf1c971e9534ab5abe80
Bea Fernandes <fernandess.beatrizz@gmail.com>
2025-03-13 14:10:15 +0000

    iniciogateway

---
+++ gateway.java
@@ -0,0 +1,14 @@
+package search;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class gateway extends UnicastRemoteObject implements interface{
+    
+}
