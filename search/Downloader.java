2430d8b8fb06791c82559cdaf59373af04caaa9c
Bea Fernandes <fernandess.beatrizz@gmail.com>
2025-03-14 20:52:36 +0000

    downloaders

---
+++ search/Downloader.java
@@ -0,0 +1,17 @@
+package search;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Downloader extends UnicastRemoteObject{
+    //vamos ter d meter aqui o nome d barrel p identificar o barrel p dps haver conexao
+    //vamos ter d conectar com os barrels
+    //indexar_resultados() e novos_url() vao ser funçoes que temos d ter
+}
