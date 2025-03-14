db7061b1d9b6b005ad75a58752acc8afd8921390
Bea Fernandes <fernandess.beatrizz@gmail.com>
2025-03-14 21:00:20 +0000

    barrels

---
+++ search/Barrels.java
@@ -0,0 +1,14 @@
+package search;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class Barrels extends UnicastRemoteObject implements BarrelsINTER{
+    //conectar os barrels
+    //indexar
+    //fazer a pesquisa
+    //nao sei s é necessario guardar o que ja temos
+}
+
+//vao se ligar a gateway atraves d RMI callback
+//têm de ter estas funçoes: fazer pesquisa e ...
---
+++ search/BarrelsINTER.java
@@ -0,0 +1,11 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+package search; 
+
+public interface BarrelsINTER extends Remote{
+    //void indexar_resultados
+    //novos_URL
+    //nao sei s temos d ver s os barrels têm informação repetida
+}
+
