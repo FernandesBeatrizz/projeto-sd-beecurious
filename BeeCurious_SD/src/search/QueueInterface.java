f7b7b7b209649f20487c3e63b4b708a3c5e5476b
reberapower1 <dianinhalindamaravilhosa@gmail.com>
2025-03-20 19:02:00 +0000

    ola boa tarde

--- BeeCurious_SD/src/search/QueueInterface.java
+++
@@ -1,9 +0,0 @@
-package search;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface QueueInterface extends Remote{
-    void putURL(String url) throws RemoteException;
-    String getURL() throws RemoteException;
-}
